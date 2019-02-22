572aa24d9247968fc2f7eb9ad8c7bff0921d6a31
inc-b <42861456+inc-b@users.noreply.github.com>
2019-02-22 11:33:32 +1100

    Update and rename FPSController.CS to Player.cs

---
+++ Player.cs
@@ -0,0 +1,16 @@
+/**
+ * Player.cs
+ * Joe Ford
+ * Created 2019-02-22
+ *
+ * Take input from various sources (eg, keyboard, mouse, gamepad) and control a player character.
+ * Allows first person and third person
+ * Extends from default creature class
+ */
+
+using UnityEngine;
+
+public class Player : Creature
+{
+
+}
