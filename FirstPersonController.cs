5b4a39ffd77c80ec9a1f6621c7443fe1a611eb40
inc-b <42861456+inc-b@users.noreply.github.com>
2019-02-27 09:25:55 +1100

    Create FirstPersonController.cs

---
+++ FirstPersonController.cs
@@ -0,0 +1,14 @@
+/**
+ * FirstPersonController.cs
+ * Joe Ford
+ * Created 2019-02-22
+ *
+ * Extends the base player class to allow a first person player character
+ * Enables controls, adds cameras, sound effects, etc
+ */
+
+using UnityEngine;
+
+public class FirstPersonController : Player {
+
+}
