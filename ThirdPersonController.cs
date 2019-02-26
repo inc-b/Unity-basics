e8c1298515d473fc116536157f1afbc320d2ab79
inc-b <42861456+inc-b@users.noreply.github.com>
2019-02-27 09:26:38 +1100

    Create ThirdPersonController.cs

---
+++ ThirdPersonController.cs
@@ -0,0 +1,14 @@
+/**
+ * ThirdPersonController.cs
+ * Joe Ford
+ * Created 2019-02-27
+ *
+ * Extends the base player class to allow a third person player character
+ * Enables controls, adds cameras, sound effects, etc
+ */
+
+using UnityEngine;
+
+public class ThirdPersonController : Player {
+
+}
