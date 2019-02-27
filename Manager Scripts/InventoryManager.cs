1daea8c33a03414dc56036d1d6561b7d477e6215
inc-b <42861456+inc-b@users.noreply.github.com>
2019-02-27 11:34:38 +1100

    Update InventoryManager.cs

--- Manager Scripts/InventoryManager.cs
+++ Manager Scripts/InventoryManager.cs
@@ -12,28 +12,28 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour {
   [SerializeField] private int maxCapacity = 3;
-    private List<Entity> containerContents = new List<Entity>();
-
-    public bool IncomingEntity(Entity newEntity) {
-        if (containerContents.Count < maxCapacity) {
-            containerContents.Add(newEntity);
-            Debug.Log("Item added");
-            return true;
-        } else {
-            Debug.Log("Container Full!");
-            return false;
-        }
+  private List<Entity> containerContents = new List<Entity>();
+  
+  public bool IncomingEntity(Entity newEntity) {
+    if (containerContents.Count < maxCapacity) {
+      containerContents.Add(newEntity);
+      Debug.Log("Item added");
+      return true;
+    } else {
+      Debug.Log("Container Full!");
+      return false;
     }
-
-    public Entity OutgoingEntity(int entitySelected) {
-        if (containerContents.Count != 0) {
-            Entity returnEntity = containerContents[entitySelected];
-            containerContents.Remove(returnEntity);
-            Debug.Log("Item taken");
-            return returnEntity;
-        } else {
-            Debug.Log("Container Empty");
-            return null;
-        }
+  }
+  
+  public Entity OutgoingEntity(int entitySelected) {
+    if (containerContents.Count != 0) {
+      Entity returnEntity = containerContents[entitySelected];
+      containerContents.Remove(returnEntity);
+      Debug.Log("Item taken");
+      return returnEntity;
+    } else {
+      Debug.Log("Container Empty");
+      return null;
     }
+  }
 }
