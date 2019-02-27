6fe441091f93293c1fee9f987bf84b88e7fa0a74
inc-b <42861456+inc-b@users.noreply.github.com>
2019-02-27 11:16:14 +1100

    Create PlayerController.cs

---
+++ Control Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+/**
+ * PlayerController.cs
+ * Joe Ford
+ * Created 2019-02-27
+ *
+ * Base player controller class that processes inputs etc
+ */
+
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+  
+}
