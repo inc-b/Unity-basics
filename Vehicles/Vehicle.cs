400d7975d33a8dd5383f979031423a6b9ffb2a35
inc-b <42861456+inc-b@users.noreply.github.com>
2019-02-27 11:27:49 +1100

    Update Vehicle.cs

--- Vehicles/Vehicle.cs
+++ Vehicles/Vehicle.cs
@@ -7,6 +7,8 @@
  * Must be placed and built like a structure, can have controls like a player, can have inventory like a storage container
  * Attributes: speed, health, turn speed, degrees of motion
  * Has a mesh and mesh collider
+ * Extension classes define the type of vehicle
+ * A vehicle can "transform" from one type to another by copying all relevant info from the vehicle, removing it, and creating a new vehicle of the required type
  */
 
 using UnityEngine;
