f0e38abccc42b35f01137f9694f28cb462e4f8f2
inc-b <42861456+inc-b@users.noreply.github.com>
2019-02-27 11:17:16 +1100

    Update and rename FirstPersonController.cs to Control Scripts/FirstPersonController.cs

---
+++ Control Scripts/FirstPersonController.cs
@@ -0,0 +1,14 @@
+/**
+ * FirstPersonController.cs
+ * Joe Ford
+ * Created 2019-02-27
+ *
+ * Extends the base player controller script to a player object to allow control of a first person player character
+ * Enables controls, adds cameras, sound effects, etc
+ */
+
+using UnityEngine;
+
+public class FirstPersonController : PlayerController {
+
+}
