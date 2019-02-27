f89b3994a1c3ba3c2ad3bf4368f8cbac236255f8
inc-b <42861456+inc-b@users.noreply.github.com>
2019-02-27 11:24:57 +1100

    Create UnderwaterVehicle.cs

---
+++ Vehicles/UnderwaterVehicle.cs
@@ -0,0 +1,14 @@
+/**
+ * UnderwaterVehicle.cs
+ * Joe Ford
+ * Created 2019-02-27
+ *
+ * Extends the base vehicle class to create an underwater vehicle
+ * eg, submarines
+ */
+
+using UnityEngine;
+
+public class UnderwaterVehicle : Vehicle {
+  
+}
