b064d0d913a01dd805b65a5027b28c3f4789194b
inc-b <42861456+inc-b@users.noreply.github.com>
2019-02-27 11:18:12 +1100

    Update and rename ThirdPersonController.cs to Control Scripts/ThirdPersonController.cs

---
+++ Control Scripts/ThirdPersonController.cs
@@ -0,0 +1,14 @@
+/**
+ * ThirdPersonController.cs
+ * Joe Ford
+ * Created 2019-02-27
+ *
+ * Extends the base player controller class to allow third person control of a player character
+ * Enables controls, adds cameras, sound effects, etc
+ */
+
+using UnityEngine;
+
+public class ThirdPersonController : PlayerController {
+
+}
