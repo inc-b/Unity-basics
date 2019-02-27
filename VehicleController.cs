860372f00a99a0ca2b41e6085357e87c90d118dc
inc-b <42861456+inc-b@users.noreply.github.com>
2019-02-27 11:03:00 +1100

    Create VehicleController.cs

---
+++ VehicleController.cs
@@ -0,0 +1,15 @@
+/**
+ * VehicleController.cs
+ * Joe Ford
+ * Created 2019-02-27
+ *
+ * Attached to a vehicle object to allow the player to control it
+ * Enables controls, adds cameras, sound effects, etc
+ * Has functions to removes the player from the world, store them, and return them to the world
+ */
+
+using UnityEngine;
+
+public class VehicleController : MonoBehaviour {
+
+}
