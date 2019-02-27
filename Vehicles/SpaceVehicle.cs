05363bb936b7e268b67a8ad5a37e153286aa845e
inc-b <42861456+inc-b@users.noreply.github.com>
2019-02-27 11:25:50 +1100

    Create SpaceVehicle.cs

---
+++ Vehicles/SpaceVehicle.cs
@@ -0,0 +1,14 @@
+/**
+ * SpaceVehicle.cs
+ * Joe Ford
+ * Created 2019-02-27
+ *
+ * Extends the base vehicle class to create a space based vehicle
+ * eg, capital ships, gunships, haulers, fighters, etc
+ */
+
+using UnityEngine;
+
+public class SpaceVehicle : Vehicle {
+  
+}
