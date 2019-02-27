f6f4fd8f44a03c08ebf64e370b4ae234573f57cc
inc-b <42861456+inc-b@users.noreply.github.com>
2019-02-27 11:24:12 +1100

    Create AirVehicle.cs

---
+++ Vehicles/AirVehicle.cs
@@ -0,0 +1,14 @@
+/**
+ * AirVehicle.cs
+ * Joe Ford
+ * Created 2019-02-27
+ *
+ * Extends the base vehicle class to create an air based vehicle
+ * eg, planes, helicopters, gliders, etc
+ */
+
+using UnityEngine;
+
+public class AirVehicle : Vehicle {
+  
+}
