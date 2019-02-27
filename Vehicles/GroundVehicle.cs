f9439e0a06dc64e240aacfa7b3236b41ed154962
inc-b <42861456+inc-b@users.noreply.github.com>
2019-02-27 11:22:15 +1100

    Create GroundVehicle.cs

---
+++ Vehicles/GroundVehicle.cs
@@ -0,0 +1,14 @@
+/**
+ * GroundVehicle.cs
+ * Joe Ford
+ * Created 2019-02-27
+ *
+ * Extends the base vehicle class to create a ground based vehicle
+ * eg, cars, motorbikes, bicycles, skateboards, etc
+ */
+
+using UnityEngine;
+
+public class GroundVehicle : Vehicle {
+  
+}
