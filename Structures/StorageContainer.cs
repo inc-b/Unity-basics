9c14637ed59cb7101b4a0a032018102a40ea504f
inc-b <42861456+inc-b@users.noreply.github.com>
2019-02-27 11:35:13 +1100

    Update StorageContainer.cs

--- Structures/StorageContainer.cs
+++ Structures/StorageContainer.cs
@@ -11,31 +11,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(InventoryManager))]
 public class StorageContainer : Structure {
-
-    [SerializeField] private int maxCapacity = 3;
-    private List<Entity> containerContents = new List<Entity>();
-
-    public bool IncomingEntity(Entity newEntity) {
-        if (containerContents.Count < maxCapacity) {
-            containerContents.Add(newEntity);
-            Debug.Log("Item added");
-            return true;
-        } else {
-            Debug.Log("Container Full!");
-            return false;
-        }
-    }
-
-    public Entity OutgoingEntity(int entitySelected) {
-        if (containerContents.Count != 0) {
-            Entity returnEntity = containerContents[entitySelected];
-            containerContents.Remove(returnEntity);
-            Debug.Log("Item taken");
-            return returnEntity;
-        } else {
-            Debug.Log("Container Empty");
-            return null;
-        }
-    }
+    
 }
