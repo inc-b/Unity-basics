62bdca27cf3f642f43821da189d1f65b5d410892
inc-b <42861456+inc-b@users.noreply.github.com>
2019-02-27 11:23:20 +1100

    Create WaterVehicle.cs

---
+++ Vehicles/WaterVehicle.cs
@@ -0,0 +1,14 @@
+/**
+ * WaterVehicle.cs
+ * Joe Ford
+ * Created 2019-02-27
+ *
+ * Extends the base vehicle class to create a water based vehicle
+ * eg, motorboats, jet skis, sailing ships, etc
+ */
+
+using UnityEngine;
+
+public class WaterVehicle : Vehicle {
+  
+}
