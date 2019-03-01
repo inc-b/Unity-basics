2de7d8632cecf44d0d08c0ce2d5391d31a6e2d00
inc-b <42861456+inc-b@users.noreply.github.com>
2019-03-02 09:42:15 +1100

    Update Creature.cs

--- Objects/Entities/Creature.cs
+++ Objects/Entities/Creature.cs
@@ -7,6 +7,8 @@
  * Has attributes like movement speed, turning speed, health, stamina, etc
  * This class has no brain - an extension of the class is required to provide control
  * Extends from default entity class
+ *
+ * A creature can move around the world, run into structures, pick entities up, drop entities
  */
  
  using UnityEngine;
@@ -14,23 +16,102 @@
 public class Creature : Entity {
     [SerializeField] private float moveSpeed = 1.0f;
     [SerializeField] private float turnSpeed = 1.0f;
+    [SerializeField] private int jumpSpeed = 5;
+
     [SerializeField] private int health = 100;
     private int currentHealth;
     [SerializeField] private int stamina = 100;
     private int currentStamina;
+
     private Entity inventory;
+    private Vector3 moveDirection;
+    private bool isMoving = true;
+    private bool touchingGround = false;
+    private bool isJumping = false;
+    private bool turningRight = false;
+    private bool turningLeft = false;
 
     private void Start() {
+        base.Start();
         currentHealth = health;
         currentStamina = stamina;
     }
+
     private void FixedUpdate() {
+        if (isMoving && touchingGround) {
+            transform.Translate(moveDirection * Time.deltaTime, Space.Self);
+        }
+        if (turningRight) {
+            transform.Rotate(0, turnSpeed, 0, Space.World);
+        }
+
+        if (turningLeft) {
+            transform.Rotate(0, -turnSpeed, 0, Space.World);
+        }
         
     }
 
+
+    // Movement functions
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("GROUND")) {
+            touchingGround = true;
+            isJumping = false;
+        }
+    }
+        
+    private void OnCollisionExit(Collision collision) {
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("GROUND")) {
+            touchingGround = false;
+        }
+    }
+
+    public void walkForward() {
+        isMoving = true;
+        moveDirection = Vector3.forward;
+    }
+
+    public void walkBackward() {
+        if (isMoving) {
+            isMoving = false;
+        } else {
+            isMoving = true;
+            moveDirection = Vector3.back;
+        }
+    }
+
+    public void turnTo(float turnAngle) {
+        if (turnAngle > 0) {
+            turningRight = true;
+            turningLeft = false;
+        } else {
+            turningRight = false;
+            turningLeft = true;
+        }
+    }
+
+    public void stopMoving() {
+        isMoving = false;
+        turningLeft = false;
+        turningRight = false;
+        moveDirection = Vector3.zero;
+    }
+
+    public void jump() {
+        if (touchingGround) {
+            isJumping = true;
+            rb.AddForce(new Vector3(0, jumpSpeed, 0), ForceMode.Impulse);
+        }
+    }
+
+
+
+    // Interaction functions
+
     private void GiveInventory(StorageContainer storageContainer) {
         if (inventory != null) {
-            bool success = storageContainer.IncomingEntity(inventory);
+            bool success = storageContainer.GetComponent<InventoryManager>().IncomingEntity(inventory);
             if (success) {
                 inventory = null;
             } else {
@@ -43,7 +124,7 @@ public class Creature : Entity {
 
     private void TakeFromContainer(StorageContainer storageContainer) {
         if (inventory == null) {
-            inventory = storageContainer.OutgoingEntity(0);
+            inventory = storageContainer.GetComponent<InventoryManager>().OutgoingEntity(0);
             Debug.Log("Taken object");
         } else {
             Debug.Log("Inventory full!");
