9180f045e5c48e1872961454cdf8220cd15aa368
inc-b <42861456+inc-b@users.noreply.github.com>
2019-03-05 12:12:11 +1100

    Update AirAnimal.cs

--- Objects/Entities/AirAnimal.cs
+++ Objects/Entities/AirAnimal.cs
@@ -1,1 +1,16 @@
+/**
+ * AirAnimal.cs
+ * Joe Ford
+ * Created 2019-03-05
+ *
+ * Flying animal
+ * Extends from default creature class
+ *
+ * A flying animal can take off, fly and land
+ */
+ 
+using UnityEngine;
 
+public class AirAnimal : Creature {
+  
+}
