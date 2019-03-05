d9ee9ecbe1df25cd416a35f1e0068c71c71d04ed
inc-b <42861456+inc-b@users.noreply.github.com>
2019-03-05 12:13:07 +1100

    Update and rename SeaAnimal.cs to WaterAnimal.cs

---
+++ Objects/Entities/WaterAnimal.cs
@@ -0,0 +1,16 @@
+/**
+ * WaterAnimal.cs
+ * Joe Ford
+ * Created 2019-03-05
+ *
+ * Underwater animal
+ * Extends from default creature class
+ *
+ * A swimming animal can breach the surface of the water and fall back in
+ */
+ 
+using UnityEngine;
+
+public class WaterAnimal : Creature {
+  
+}
