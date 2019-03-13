a5f31ac1d45414491cf361cb8f2f100c90de6f83
inc-b <42861456+inc-b@users.noreply.github.com>
2019-03-13 17:47:39 +1100

    Create AmoebaAI.cs

---
+++ Scripts/AI/AmoebaAI.cs
@@ -0,0 +1,19 @@
+
+/**
+ * AmoebaAI.cs
+ * Joe Ford
+ * Created 2019-03-13
+ *
+ * The simplest NPC AI. Looks for food, moves towards the food, eats the food.
+ * If there's no food in sight then it will wander in a non-random way.
+ *
+ * STATES
+ * Wander: Move around the world, waiting for a state change to be triggered.
+ * Eat: Eat the nearest piece of food.
+ */
+
+using UnityEngine;
+
+public class AmoebaAI : MonoBehaviour {
+
+}
